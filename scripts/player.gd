e091b9d3e701c409b407360d13fbe3093466812f
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-03-20 19:46:14 -0300

    Máquina de Estados Simples para o Player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,91 @@
 extends CharacterBody2D
 
+enum PlayerEstate {
+	idle,
+	walk, 
+	jump
+}
+
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
+var status: PlayerEstate
+
+func _ready() -> void:
+	go_to_idle_state()
+
 func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	match status:
+		PlayerEstate.idle:
+			idle_state()
+		PlayerEstate.walk:
+			walk_state()
+		PlayerEstate.jump:
+			jump_state()
+			
+	move_and_slide()
+
+func go_to_idle_state():
+	status = PlayerEstate.idle
+	anim.play("idle")
+
+func go_to_walk_state():
+	status = PlayerEstate.walk
+	anim.play("walk")
+	
+func go_to_jump_state():
+	status = PlayerEstate.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+func idle_state():
+	move()
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+func walk_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()
+		return
+	
+func move():
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+
+func temp(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,25 +93,3 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if is_on_floor():
-		if direction > 0:
-			anim.flip_h = false
-			anim.play("walk")
-		elif direction < 0:
-			anim.flip_h = true
-			anim.play("walk")
-		else:
-			anim.play("idle")
-	else:
-		anim.play("jump")
-
-	move_and_slide()
