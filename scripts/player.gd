95d4fc8e19ede8ceefa33b077208b4e0f8cca837
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-03-23 11:59:01 -0300

    Movimentação Suave - Acelerando e Desacelerando o Personagem

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,13 +11,23 @@ enum PlayerState {
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
-const SPEED = 80.0
+@export var max_speed = 180.0
+@export var acceleration = 100
+@export var deceleration = 100
 const JUMP_VELOCITY = -300.0
 
 var jump_count = 0
 @export var max_jump_count = 2
 var direction = 0
 var status: PlayerState
+
+func move(delta):
+	update_direction()
+	
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
 func _ready() -> void:
 	go_to_idle_state()
@@ -29,15 +39,15 @@ func _physics_process(delta: float) -> void:
 	
 	match status:
 		PlayerState.idle:
-			idle_state()
+			idle_state(delta)
 		PlayerState.walk:
-			walk_state()
+			walk_state(delta)
 		PlayerState.jump:
-			jump_state()
+			jump_state(delta)
 		PlayerState.fall:
-			fall_state()
+			fall_state(delta)
 		PlayerState.duck:
-			duck_state()
+			duck_state(delta)
 			
 	move_and_slide()
 
@@ -71,8 +81,8 @@ func exit_from_duck_state():
 	collision_shape.shape.height = 16
 	collision_shape.position.y = 0
 
-func idle_state():
-	move()
+func idle_state(delta):
+	move(delta)
 	if velocity.x != 0:
 		go_to_walk_state()
 		return
@@ -85,8 +95,8 @@ func idle_state():
 		go_to_duck_state()
 		return
 	
-func walk_state():
-	move()
+func walk_state(delta):
+	move(delta)
 	if velocity.x == 0:
 		go_to_idle_state()
 		return
@@ -100,8 +110,8 @@ func walk_state():
 		go_to_fall_state()
 		return
 	
-func jump_state():
-	move()
+func jump_state(delta):
+	move(delta)
 	
 	if Input.is_action_just_pressed("jump") && can_jump():
 		go_to_jump_state()
@@ -111,8 +121,8 @@ func jump_state():
 		go_to_fall_state()
 		return
 		
-func fall_state():
-	move()
+func fall_state(delta):
+	move(delta)
 	
 	if Input.is_action_just_pressed("jump") && can_jump():
 		go_to_jump_state()
@@ -126,21 +136,13 @@ func fall_state():
 			go_to_walk_state()
 		return
 		
-func duck_state():
+func duck_state(delta):
 	update_direction()
 	if Input.is_action_just_released("duck"):
 		exit_from_duck_state()
 		go_to_idle_state()
 		return
-	
-func move():
-	update_direction()
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		
 func update_direction():
 	direction = Input.get_axis("left", "right")
 	
