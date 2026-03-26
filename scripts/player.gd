563d99fe8ee0480b9dc37a66717d95c9231a6da4
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-03-26 12:22:19 -0300

    Reload da Cena e Ajustes Importantes Antes do Ataque do Inimigo

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,7 @@ enum PlayerState {
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var reload_timer: Timer = $ReloadTimer
 
 @export var max_speed = 180.0
 @export var acceleration = 400
@@ -88,6 +89,7 @@ func go_to_dead_state():
 	status = PlayerState.dead
 	anim.play("dead")
 	velocity = Vector2.ZERO
+	reload_timer.start()
 
 func idle_state(delta):
 	move(delta)
@@ -207,4 +209,8 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 		go_to_jump_state()
 	else:
 		# Player morre
-		go_to_dead_state()
+		if status != PlayerState.dead:
+			go_to_dead_state()
+
+func _on_reload_timer_timeout() -> void:
+	get_tree().reload_current_scene()
