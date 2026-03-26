783c330ca39297ccd5a4281e418a0bcf7d4c6916
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-03-26 13:47:27 -0300

    Como Atirar ou Arremessar Projéteis ou Dropar Itens com Precisão

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 enum SkeletonState {
 	walk,
+	attack,
 	dead
 }
+
+const SPINNING_BONE = preload("res://entities/spinning_bone.tscn")
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var wall_detector: RayCast2D = $WallDetector
 @onready var ground_detector: RayCast2D = $GroundDetector
+@onready var player_detector: RayCast2D = $PlayerDetector
+@onready var bone_start_position: Node2D = $BoneStartPosition
 
 const SPEED = 10.0
 const JUMP_VELOCITY = -400.0
@@ -16,6 +21,7 @@ const JUMP_VELOCITY = -400.0
 var status: SkeletonState
 
 var direction = 1
+var can_throw = true
 
 func _ready() -> void:
 	go_to_walk_state()
@@ -30,12 +36,20 @@ func _physics_process(delta: float) -> void:
 			walk_state(delta)
 		SkeletonState.dead:
 			dead_state(delta)
+		SkeletonState.attack:
+			attack_state(delta)
 
 	move_and_slide()
 
 func go_to_walk_state():
 	status = SkeletonState.walk
 	anim.play("walk")
+	
+func go_to_attack_state():
+	status = SkeletonState.attack
+	anim.play("attack")
+	velocity = Vector2.ZERO
+	can_throw = true
 	
 func go_to_dead_state():
 	status = SkeletonState.dead
@@ -54,8 +68,28 @@ func walk_state(_delta):
 		scale.x *= -1
 		direction *= -1
 		
+	if player_detector.is_colliding():
+		go_to_attack_state()
+		return
+		
+func attack_state(_delta):
+	if anim.frame == 2 && can_throw:
+		throw_bone()
+		can_throw = false
+		
 func dead_state(_delta):
 	pass
 	
 func take_damage():
 	go_to_dead_state()
+	
+func throw_bone():
+	var new_bone = SPINNING_BONE.instantiate()
+	add_sibling(new_bone)
+	new_bone.position = bone_start_position.global_position
+	new_bone.set_direction(self.direction)
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.animation == "attack":
+		go_to_walk_state()
+		return
--- scripts/spinning_bone.gd
+++ scripts/spinning_bone.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
-var speed = 100
-var direction = -1
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+var speed = 60
+var direction = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x += speed * delta * direction
+
+func set_direction(direction):
+	self.direction = direction
+	anim.flip_h = direction < 0
+	
