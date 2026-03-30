724b1597ed7e3848b968b318521178706c7c1429
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-03-30 15:43:37 -0300

    Controle e Física de Nado: Movimento Suave Debaixo da Água

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,6 +25,8 @@ enum PlayerState {
 @export var slide_deceleration = 100
 @export var wall_acceleration = 40
 @export var wall_jump_velocity = 240
+@export var water_max_speed = 100
+@export var water_acceleration = 200
 
 const JUMP_VELOCITY = -300.0
 
@@ -103,6 +105,7 @@ func go_to_wall_state():
 func go_to_swimming_state():
 	status = PlayerState.swimming
 	anim.play("swimming")
+	velocity.y = min(velocity.y, 150)
 	
 func go_to_dead_state():
 	if status == PlayerState.dead:
@@ -231,7 +234,18 @@ func wall_state(delta):
 		return
 		
 func swimming_state(delta):
-	pass
+	update_direction()
+	
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, water_max_speed * direction, water_acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, water_acceleration * delta)
+		
+	var vertical_direction = Input.get_axis("jump", "duck")
+	if vertical_direction:
+		velocity.y = move_toward(velocity.y, water_max_speed * vertical_direction, water_acceleration * delta)
+	else:
+		velocity.y = move_toward(velocity.y, 0, water_acceleration * delta)
 		
 func dead_state(delta):
 	apply_gravity(delta)
@@ -301,3 +315,8 @@ func hit_lethal_area():
 
 func _on_reload_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Water"):
+		jump_count = 0
+		go_to_jump_state()
