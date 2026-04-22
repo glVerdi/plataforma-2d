7e517b3cc0de8c67a2ca13b0341fba6e8f2a0c3d
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-04-22 18:22:43 -0300

    Spinning_bone update
    Colocando o spinning bone para a mesma camada do esqueleto

--- scripts/spinning_bone.gd
+++ scripts/spinning_bone.gd
@@ -5,6 +5,9 @@ extends Area2D
 var speed = 60
 var direction = 1
 
+func _ready() -> void:
+	z_index = 1
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x += speed * delta * direction
