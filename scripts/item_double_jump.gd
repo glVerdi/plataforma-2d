41d344ad0c246d9c18b249a00e4032337057164f
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-06-22 22:20:40 -0300

    Adicionando a luva para o wall jump
    Coloquei um sprite para pegar uma luva e ter wall jump na fase 2

--- scripts/item_double_jump.gd
+++ scripts/item_double_jump.gd
@@ -1,16 +1,18 @@
 extends Area2D
 
+@onready var sprite: Sprite2D = $Sprite2D
+var tempo: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	tempo += delta
+	sprite.position.y = sin(tempo * 3.0) * 4.0
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player" or body.is_in_group("player"):
-		GameManeger.has_double_jump = true
+		GameManeger.has_wall_jump = true
 		queue_free()
