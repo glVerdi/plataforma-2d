b3cfa050c28b79e54b94462009be2b179378221b
Gabriel Leoncio Verdi <gabrielverdi12@gmail.com>
2026-07-04 23:02:07 -0300

    Tela de vitória
    Criando a tela de vit´ria e colocando ela no level end da fase forest

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -9,15 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/title_screen.tscn")
 	
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/tropic.tscn")
-
-func _on_credits_button_pressed() -> void:
-	CreditsDialog.popup_centered(Vector2(400, 250))
-
 func _on_quite_button_pressed() -> void:
 	get_tree().quit()
-
-func _on_rich_text_label_meta_clicked(meta: Variant) -> void:
-	OS.shell_open(str(meta))
